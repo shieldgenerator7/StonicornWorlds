df336a9d07624a40fbd2b94387a715a887d6b92b
shieldgenerator7 <shieldgenerator7@yahoo.com>
2021-04-28 04:06:52 -0400

    Camera zooms out to keep pods in view

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -17,12 +17,29 @@ public class CameraController : MonoBehaviour
     {
         float minX = posList.Min(pos => pos.x);
         float minY = posList.Min(pos => pos.y);
+        Vector2 min = new Vector2(minX, minY);
         float maxX = posList.Max(pos => pos.x);
         float maxY = posList.Max(pos => pos.y);
+        Vector2 max = new Vector2(maxX, maxY);
         Camera.main.transform.position = new Vector3(
             (minX + maxX) / 2,
             (minY + maxY) / 2,
             Camera.main.transform.position.z
             );
+        Camera.main.orthographicSize = 5;
+        while (!areVectorsInFrame(posList, 1))
+        {
+            Camera.main.orthographicSize += 0.1f;
+        }
+    }
+
+    public bool areVectorsInFrame(List<Vector2> vectors, float buffer)
+    {
+        Bounds b = new Bounds();
+        b.min = (Vector2)Camera.main.ViewportToWorldPoint(Vector2.zero);
+        b.min += (Vector3)Vector2.one * buffer;
+        b.max = (Vector2)Camera.main.ViewportToWorldPoint(Vector2.one);
+        b.max -= (Vector3)Vector2.one * buffer;
+        return vectors.TrueForAll(v => b.Contains(v));
     }
 }
