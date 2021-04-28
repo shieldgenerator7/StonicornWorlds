b65991bdb1f9199f99cbcb47d283fd09dcd9e60d
shieldgenerator7 <shieldgenerator7@yahoo.com>
2021-04-27 21:16:37 -0400

    Refactor PlanetManager

--- Assets/Scripts/PlanetManager.cs
+++ Assets/Scripts/PlanetManager.cs
@@ -5,17 +5,37 @@ using UnityEngine;
 
 public class PlanetManager : MonoBehaviour
 {
-    public GameObject addPodPrefab;
+    List<Pod> pods = new List<Pod>();
+    List<Vector2> addPosList = new List<Vector2>();
 
-    public GameObject podPrefab;
-    public List<Pod> pods = new List<Pod>();
-    public List<GameObject> goPods = new List<GameObject>();
-    public List<GameObject> addPods = new List<GameObject>();
+    public delegate void AddPosListChanged(List<Vector2> list);
+    public AddPosListChanged addPosListChanged;
+    public delegate void PodsListChanged(List<Pod> list);
+    public PodsListChanged podsListChanged;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        updateDisplay();
+        addPosList.Add(Vector2.zero);
+        addPosListChanged?.Invoke(addPosList);
+        podsListChanged += (list) =>
+        {
+            List<Vector2> posList = list.ConvertAll(pod => pod.pos);
+            addPosList = new List<Vector2>();
+            foreach (Vector2 pos in posList)
+            {
+                List<Vector2> adjs = getAdjacentPositions(pos);
+                foreach (Vector2 adj in adjs)
+                {
+                    if (!posList.Contains(adj) && !addPosList.Contains(adj))
+                    {
+                        addPosList.Add(adj);
+                    }
+                }
+            }
+            addPosListChanged?.Invoke(addPosList);
+        };
     }
 
     // Update is called once per frame
@@ -24,60 +44,13 @@ public class PlanetManager : MonoBehaviour
         if (Input.GetMouseButtonDown(0))
         {
             Vector2 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            GameObject clickedUI = addPods.FirstOrDefault(go => checkAddPodUI(pos, go));
-            if (clickedUI)
+            Vector2 clickedUI = addPosList.FirstOrDefault(addPos => checkAddPodUI(pos, addPos));
+            if (clickedUI != Vector2.zero || addPosList.Count == 1)
             {
-                pods.Add(new Pod(clickedUI.transform.position));
-                updateDisplay();
+                pods.Add(new Pod(clickedUI));
+                podsListChanged?.Invoke(pods);
             }
         }
-    }
-
-    public void updateDisplay()
-    {
-        //Update pods
-        goPods.ForEach(go => Destroy(go));
-        goPods.Clear();
-        pods.ForEach(pod =>
-        {
-            GameObject go = Instantiate(
-                podPrefab,
-                pod.pos,
-                Quaternion.identity,
-                transform
-                );
-            goPods.Add(go);
-        });
-        //Update add pod UI
-        addPods.ForEach(go => Destroy(go));
-        addPods.Clear();
-        List<Vector2> posList = pods.ConvertAll(pod => pod.pos);
-        List<Vector2> edges = new List<Vector2>();
-        foreach (Vector2 pos in posList)
-        {
-            List<Vector2> adjs = getAdjacentPositions(pos);
-            foreach (Vector2 adj in adjs)
-            {
-                if (!posList.Contains(adj) && !edges.Contains(adj))
-                {
-                    edges.Add(adj);
-                }
-            }
-        }
-        if (edges.Count == 0)
-        {
-            edges.Add(Vector2.zero);
-        }
-        edges.ForEach(edge =>
-        {
-            GameObject addPod = Instantiate(
-                addPodPrefab,
-                edge,
-                Quaternion.identity,
-                transform
-                );
-            addPods.Add(addPod);
-        });
     }
 
     public List<Vector2> getAdjacentPositions(Vector2 pos)
@@ -94,9 +67,9 @@ public class PlanetManager : MonoBehaviour
         return dirs.ConvertAll(dir => dir + pos);
     }
 
-    public bool checkAddPodUI(Vector2 pos, GameObject addPodUI)
+    public bool checkAddPodUI(Vector2 pos1, Vector2 pos2)
     {
-        SpriteRenderer sr = addPodUI.GetComponent<SpriteRenderer>();
-        return sr.bounds.Contains(pos);
+        float radius = 0.5f;
+        return Vector2.Distance(pos1, pos2) <= radius;
     }
 }
---
+++ Assets/Scripts/PlanetManagerEffects.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetManagerEffects : MonoBehaviour
+{
+    public PlanetManager planetManager;
+
+    public GameObject addPodPrefab;
+    public GameObject podPrefab;
+
+    List<GameObject> goPods = new List<GameObject>();
+    List<GameObject> addPods = new List<GameObject>();
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        planetManager.podsListChanged += updateDisplay;
+
+        planetManager.addPosListChanged += updateAddDisplay;
+    }
+
+    public void updateDisplay(List<Pod> pods)
+    {
+        //Update pods
+        goPods.ForEach(go => Destroy(go));
+        goPods.Clear();
+        pods.ForEach(pod =>
+        {
+            GameObject go = Instantiate(
+                podPrefab,
+                pod.pos,
+                Quaternion.identity,
+                transform
+                );
+            goPods.Add(go);
+        });
+    }
+    public void updateAddDisplay(List<Vector2> edges)
+    {
+        //Update add pod UI
+        addPods.ForEach(go => Destroy(go));
+        addPods.Clear();
+        edges.ForEach(edge =>
+        {
+            GameObject addPod = Instantiate(
+                addPodPrefab,
+                edge,
+                Quaternion.identity,
+                transform
+                );
+            addPods.Add(addPod);
+        });
+    }
+}
