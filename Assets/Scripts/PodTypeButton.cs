0b6f903a80de8ea7bd4077920bc4bf345bac7a5c
shieldgenerator7 <shieldgenerator7@yahoo.com>
2021-04-29 22:11:48 -0400

    Move pod type selection buttons into UI

--- Assets/Scripts/PodTypeButton.cs
+++ Assets/Scripts/PodTypeButton.cs
@@ -1,31 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PodTypeButton : MonoBehaviour
 {
     public PodType podType;
-
-    GameObject pod;
+    public Image image;
+    Vector2 position;
 
     // Start is called before the first frame update
     void Start()
     {
-        pod = Instantiate(podType.podPrefab, transform);
+        SpriteRenderer sr = podType.podPrefab.GetComponent<SpriteRenderer>();
+        image.sprite = sr.sprite;
+        image.color = sr.color;
+        RectTransform rect = GetComponent<RectTransform>();
+        position = rect.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0) && checkClick(Camera.main.ScreenToWorldPoint(Input.mousePosition)))
+        if (Input.GetMouseButtonDown(0) && checkClick(Input.mousePosition))
         {
-            FindObjectOfType<PlanetManager>().PodType = podType;
+            setPodType();
         }
     }
 
     public bool checkClick(Vector2 pos)
     {
-        SpriteRenderer sr = pod.GetComponent<SpriteRenderer>();
-        return sr.bounds.Contains(pos);
+        return Vector2.Distance(pos, position) <= 50;
     }
+
+    public void setPodType()
+    {
+        FindObjectOfType<PlanetManager>().PodType = podType;
+    }
+
 }
