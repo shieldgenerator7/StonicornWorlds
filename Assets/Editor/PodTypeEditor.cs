52314db6c927e88cc5e705f00b537a7ce7193356
shieldgenerator7 <shieldgenerator7@yahoo.com>
2021-05-05 00:14:03 -0400

    Add PodType editor

---
+++ Assets/Editor/PodTypeEditor.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(PodType))]
+[CanEditMultipleObjects]
+public class PodTypeEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+        if (GUILayout.Button("Setup pod type"))
+        {
+            foreach (Object t in targets)
+            {
+                PodType pt = (PodType)t;
+                pt.typeName = pt.name;
+                pt.podPrefab = AssetDatabase.LoadAssetAtPath<GameObject>(
+                    "Assets/Prefabs/Pod " + pt.typeName + ".prefab"
+                    );
+                pt.preview = AssetDatabase.LoadAssetAtPath<Sprite>(
+                    "Assets/Sprites/pod_" + pt.typeName.ToLower() + ".png"
+                    );
+                pt.uiColor = pt.podPrefab.GetComponent<SpriteRenderer>().color;
+                EditorUtility.SetDirty(pt);
+            }
+        }
+    }
+}
