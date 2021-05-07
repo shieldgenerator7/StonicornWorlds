87bba19d226de73ee697f2d224f6eee72828b906
shieldgenerator7 <shieldgenerator7@yahoo.com>
2021-05-07 03:30:38 -0400

    Fix issue: Unity enter play mode infinite loop

--- Assets/Scripts/Planet.cs
+++ Assets/Scripts/Planet.cs
@@ -4,8 +4,14 @@ using UnityEngine;
 
 public class Planet : MonoBehaviour
 {
-    private HexagonGrid<Pod> grid = new HexagonGrid<Pod>();
-    private HexagonGrid<Pod> futureGrid = new HexagonGrid<Pod>();
+    private HexagonGrid<Pod> grid;
+    private HexagonGrid<Pod> futureGrid;
+
+    private void Awake()
+    {
+        grid = new HexagonGrid<Pod>();
+        futureGrid = new HexagonGrid<Pod>();
+    }
 
     #region Write State
     public delegate void OnStateChanged(Planet p);
