4850a21603a396ea20fd4d2ef52a58412c9e9d34
shieldgenerator7 <shieldgenerator7@yahoo.com>
2021-05-12 16:52:21 -0400

    Add ToolAction compatibility

--- Assets/Scripts/PlanetObjectTypeButton.cs
+++ Assets/Scripts/PlanetObjectTypeButton.cs
@@ -6,8 +6,14 @@ public class PlanetObjectTypeButton : ToolButton
 {
     public PlanetObjectType planetObjectType;
 
+    public List<ToolAction> compatibleToolActions;
+
     public override void activate()
     {
         Managers.Input.PlanetObjectType = planetObjectType;
+        if (!compatibleToolActions.Contains(Managers.Input.ToolAction))
+        {
+            Managers.Input.ToolAction = compatibleToolActions[0];
+        }
     }
 }
