a780cfb4a58c56424c51f3fae451d8993a6a827f
shieldgenerator7 <shieldgenerator7@yahoo.com>
2021-05-14 15:31:53 -0400

    Add ResourceGenerator*
    *replaces ResourceMine

---
+++ Assets/Scripts/ResourceGenerator.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourceGenerator : MonoBehaviour
+{
+    public PodType generatorPodType;
+    public float generateRate = 1;
+    private int count = 0;
+
+    private void Start()
+    {
+        Managers.Planet.onPlanetStateChanged += updateCount;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Managers.Planet.Resources += count * generateRate * Time.deltaTime;
+    }
+
+    void updateCount(Planet planet)
+    {
+        count = planet.Pods(generatorPodType).Count;
+        enabled = count > 0;
+    }
+}
