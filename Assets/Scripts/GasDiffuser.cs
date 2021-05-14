717f1db46922aaab03d308b59035f639e1eb062f
shieldgenerator7 <shieldgenerator7@yahoo.com>
2021-05-14 04:00:17 -0400

    Rename SkySpreader -> GasDiffuser

--- Assets/Scripts/GasDiffuser.cs
+++ Assets/Scripts/GasDiffuser.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-public class SkySpreader : MonoBehaviour
+public class GasDiffuser : MonoBehaviour
 {
     public float diffusionRate = 5;
     public float minAmount = 10;//min pressure amount to start diffusing
