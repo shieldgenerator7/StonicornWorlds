993ae8b1accd7055bf485303b74f2139ea805e84
shieldgenerator7 <shieldgenerator7@yahoo.com>
2021-05-22 16:25:39 -0400

    Update build display info

--- Assets/Scripts/FileManager.cs
+++ Assets/Scripts/FileManager.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 public class FileManager : MonoBehaviour
 {
-    public string fileName = "planet1.wrld";
+    public string fileName = "";
 
     public void setup()
     {
@@ -18,16 +18,13 @@ public class FileManager : MonoBehaviour
 
     void SaveFile()
     {
-        Debug.Log("SAVE");
         Managers.Planet.Planet.tasks = Managers.Queue.Tasks;
         ES3.Save<string>("planet", JsonUtility.ToJson(Managers.Planet.Planet), fileName);
         ES3.Save<float>("resources", Managers.Planet.Resources, fileName);
-        Debug.Log("SAVED");
     }
 
     void LoadFile()
     {
-        Debug.Log("LOAD");
         if (ES3.FileExists(fileName))
         {
             //Planet
@@ -41,7 +38,6 @@ public class FileManager : MonoBehaviour
             //Resources
             float resources = ES3.Load<float>("resources", fileName);
             Managers.Planet.Resources = resources;
-            Debug.Log("LOADED");
         }
     }
 }
