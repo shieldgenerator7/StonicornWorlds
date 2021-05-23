4b3fc4aad74f4ecf8e866e2e1841efd44c06eaad
shieldgenerator7 <shieldgenerator7@yahoo.com>
2021-05-23 17:23:10 -0400

    Remove test code

--- Assets/Scripts/MagmaContainerDisplayer.cs
+++ Assets/Scripts/MagmaContainerDisplayer.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class MagmaContainerDisplayer : PodContentDisplayer
 {
-    public float val = 0;
     public float circleRadius = 0.13f;
     public List<SpriteRenderer> srs;
     private List<Vector2> originalPosList;
-    private float prevVal = 0;
 
     public override void setup(PodContent content)
     {
         originalPosList = srs.ConvertAll(sr => (Vector2)sr.transform.position);
+        for (int i = 0; i < srs.Count; i++)
+        {
+            Vector2 dir = Managers.Planet.Planet.position - originalPosList[i];
+            srs[i].transform.up = -dir;
+            srs[i].transform.position = originalPosList[i] + (dir * circleRadius);
+        }
         //
         content.onVarChanged += updateDisplay;
         updateDisplay(content.Var);
@@ -20,14 +24,6 @@ public class MagmaContainerDisplayer : PodContentDisplayer
 
     void updateDisplay(float val)
     {
-        this.val = val;
-        for (int i = 0; i < srs.Count; i++)
-        {
-            Vector2 dir = Managers.Planet.Planet.position - originalPosList[i];
-            srs[i].transform.up = -dir;
-            srs[i].transform.position = originalPosList[i] + (dir * circleRadius);
-        }
-
         float max = Managers.Resources.magmaCapPerCore;
         float share = max / srs.Count;
         for (int i = 0; i < srs.Count; i++)
@@ -37,16 +33,6 @@ public class MagmaContainerDisplayer : PodContentDisplayer
             size.y = Mathf.Clamp(val / share, 0.01f, 1);
             fillSR.size = size;
             val -= share;
-            Debug.Log("set size: " + size.y + ", " + share);
-        }
-    }
-
-    private void Update()
-    {
-        if (prevVal != val)
-        {
-            prevVal = val;
-            updateDisplay(val);
         }
     }
 }
