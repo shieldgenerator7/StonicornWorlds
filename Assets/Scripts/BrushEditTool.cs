00759b925eefffb422cf49ff57038929dc8cc833
shieldgenerator7 <shieldgenerator7@yahoo.com>
2021-05-26 15:00:30 -0400

    Fix error: startPos serialized twice in BrushEditTool*
    *very minor, but seeing the same error message in the editor all the time annoys me, so I fixed it

--- Assets/Scripts/BrushEditTool.cs
+++ Assets/Scripts/BrushEditTool.cs
@@ -5,17 +5,17 @@ using UnityEngine;
 
 public class BrushEditTool : SelectTool
 {
-    private Vector2 startPos;
+    private Vector2 startBrushPos;
     private List<Vector2> posList = new List<Vector2>();
 
     protected override List<Vector2> getSelectList(Vector2 start, Vector2 end)
     {
         Vector2 startHex = Managers.Planet.Planet.getHexPos(start);
         Vector2 endHex = Managers.Planet.Planet.getHexPos(end);
-        if (this.startPos != startHex)
+        if (this.startBrushPos != startHex)
         {
-            Debug.Log("start: " + startPos + ", new start: " + startHex);
-            this.startPos = startHex;
+            Debug.Log("start: " + startBrushPos + ", new start: " + startHex);
+            this.startBrushPos = startHex;
             posList.Clear();
             posList.Add(startHex);
         }
