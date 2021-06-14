1398f75794443c30b444855afb0fb240e03b08aa
shieldgenerator7 <shieldgenerator7@yahoo.com>
2021-06-14 03:14:56 -0400

    Add MultiGroupedList

---
+++ Assets/Scripts/MultiGroupedList.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class MultiGroupedList<T, K>
+{
+    Dictionary<T, List<K>> list = new Dictionary<T, List<K>>();
+
+    public MultiGroupedList(GroupFunction groupFunction)
+    {
+        this.groupFunction = groupFunction;
+    }
+
+    public delegate List<T> GroupFunction(K val);
+    private GroupFunction groupFunction;
+
+    public void Add(K val)
+    {
+        getLists(val)
+            .ForEach(list => list.Add(val));
+    }
+
+    public void AddAll(List<K> vals)
+    {
+        vals.ForEach(val => Add(val));
+    }
+
+    public void Update(K val)
+    {
+        list.Values.ToList().ForEach(l => l.Remove(val));
+        Add(val);
+    }
+
+    public void Remove(K val)
+    {
+        getLists(val)
+            .ForEach(list => list.Remove(val));
+    }
+    private List<List<K>> getLists(K val)
+    {
+        List<T> keys = groupFunction(val);
+        return GetLists(keys);
+    }
+
+    public List<K> GetList(T key)
+    {
+        if (!list.ContainsKey(key))
+        {
+            list.Add(key, new List<K>());
+        }
+        return list[key];
+    }
+
+    public List<List<K>> GetLists(List<T> keys)
+    {
+        return keys.ConvertAll(key => GetList(key));
+    }
+}
