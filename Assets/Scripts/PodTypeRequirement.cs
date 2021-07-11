15d05ddc093054de67dd66cbcf339688d1949eb8
shieldgenerator7 <shieldgenerator7@yahoo.com>
2021-07-11 15:50:39 -0400

    Remove require none option

--- Assets/Scripts/PodTypeRequirement.cs
+++ Assets/Scripts/PodTypeRequirement.cs
@@ -6,13 +6,9 @@ public class PodTypeRequirement : Requirement
 {
     public PodType podType;
     public int count;
-    [Tooltip("Does it require that the max amount of the given podtype is 0?")]
-    public bool requireNone = false;
 
     public override bool isRequirementMet()
         => (podType != null)
-            ? (requireNone) 
-                ? Managers.Planet.Planet.Pods(podType).Count == 0 
-                : Managers.Planet.Planet.Pods(podType).Count >= count
+            ? Managers.Planet.Planet.Pods(podType).Count >= count
             : Managers.Planet.Planet.PodsNotEmpty.Count >= count;
 }
