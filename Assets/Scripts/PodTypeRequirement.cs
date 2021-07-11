349ea4b17409a077b317c8146167bc3c554ef217
shieldgenerator7 <shieldgenerator7@yahoo.com>
2021-07-11 15:24:55 -0400

    Add Lava2

--- Assets/Scripts/PodTypeRequirement.cs
+++ Assets/Scripts/PodTypeRequirement.cs
@@ -6,9 +6,13 @@ public class PodTypeRequirement : Requirement
 {
     public PodType podType;
     public int count;
+    [Tooltip("Does it require that the max amount of the given podtype is 0?")]
+    public bool requireNone = false;
 
     public override bool isRequirementMet()
         => (podType != null)
-            ? Managers.Planet.Planet.Pods(podType).Count >= count
+            ? (requireNone) 
+                ? Managers.Planet.Planet.Pods(podType).Count == 0 
+                : Managers.Planet.Planet.Pods(podType).Count >= count
             : Managers.Planet.Planet.PodsNotEmpty.Count >= count;
 }
